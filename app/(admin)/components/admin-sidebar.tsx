74264bbf2d62778c1f2bbfd7bc42577aa248a04f
RomanDenysov <romandenysovsk@gmail.com>
2025-05-19 20:05:49 +0200

    feat: enhance admin sidebar with mocked user navigation and footer components

--- app/(admin)/components/admin-sidebar.tsx
+++ app/(admin)/components/admin-sidebar.tsx
@@ -3,12 +3,14 @@ import type { ReactNode } from 'react';
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarHeader,
   SidebarInset,
 } from '@/components/ui/sidebar';
 import { NavHeader } from './nav-header';
 import { NavMain } from './nav-main';
 import { NavSecondary } from './nav-secondary';
+import { NavUser } from './nav-user';
 import { Search } from './search';
 
 export type AdminSidebarProps = {
@@ -25,9 +27,12 @@ export default function AdminSidebar({ children }: AdminSidebarProps) {
         <Search />
         <SidebarContent>
           <NavMain />
+
           <NavSecondary className="mt-auto" />
         </SidebarContent>
-        {/* <SidebarFooter></SidebarFooter> */}
+        <SidebarFooter>
+          <NavUser />
+        </SidebarFooter>
       </Sidebar>
       <SidebarInset>{children}</SidebarInset>
     </>
