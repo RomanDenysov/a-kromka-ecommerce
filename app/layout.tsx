870801dd1bbc789c4a8d74d57dc8605ba33efa0b
RomanDenysov <romandenysovsk@gmail.com>
2025-05-19 17:42:02 +0200

    chore: add initial component structure and dependencies with shadcn integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
+import { ReactNode } from "react";
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { fonts } from "./fonts";
+
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,12 +14,12 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(fonts)}
       >
         {children}
       </body>
